ddb37dcb0a9ccfc6b42a5ee66142d6acbe46ff83
RFXXS-DEV <rfxxs.dev@gmail.com>
2026-06-08 15:54:27 -0300

    Aprendendo alguns conceitos de herança

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import br.com.alura.catalogoDeFilmes.modelos.Conteudo.Filmes;
+import br.com.alura.catalogoDeFilmes.modelos.Conteudo.Series;
 
 public class Main {
 
@@ -9,6 +10,7 @@ public class Main {
         favorito.setAnoDeLancamento(1999);
         favorito.setDuracaoEmMinutos(135);
         favorito.setIncluidoNoPlano(true);
+        System.out.println("Duração do Filme: " + favorito.getDuracaoEmMinutos());
 
         favorito.exibeFichaTecnica();
         favorito.avalia(9);
@@ -18,6 +20,17 @@ public class Main {
         System.out.println("Total de avaliações: " + favorito.getTotalDeAvaliacoes());
 
         System.out.println(String.format("Média de avaliações do filme: %.1f",favorito.pegaMedia()));
+
+        Series lost = new Series();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporadas(10);
+        System.out.println("Duração do Filme: " + lost.getDuracaoEmMinutos());
+
     }
+
+
 
 }
